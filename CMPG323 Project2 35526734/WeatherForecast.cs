b73fb38c3fe21eada5a9c9e845264fde2cbe268a
Niel Nortier <student.nielnortier@gmail.com>
2022-09-06 12:00:57 +0200

    Add project files.

---
+++ CMPG323 Project2 35526734/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CMPG323_Project2_35526734
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
